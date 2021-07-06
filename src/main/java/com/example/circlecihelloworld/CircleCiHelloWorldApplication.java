e8919cb3b5eff125d73094cdbe9ba73f86a37592
adme1664 <adme1664@yahoo.fr>
2021-07-06 07:59:11 -0400

    adding spring hello-world project

---
+++ src/main/java/com/example/circlecihelloworld/CircleCiHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.circlecihelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiHelloWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/circlecihelloworld/CircleCiHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circlecihelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
